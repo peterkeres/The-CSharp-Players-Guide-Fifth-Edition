22192f431b7058620a287704b938a4372fefe891
Peter K <pkeresdev@gmail.com>
2023-08-09 13:28:22 -0400

    Adding in Defense of Consolas project

---
+++ Defense of Consolas/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Defense_of_Consolas // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int row, column;
+            Console.Title = "Defense of Consolas";
+
+            Console.Write("Target Row? ");
+            row = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Target Column? ");
+            column = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Deploy to:");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"({row},{column-1})\n" +
+                              $"({row-1},{column})\n" +
+                              $"({row},{column+1})\n" +
+                              $"({row+1},{column})");
+            
+            Console.Beep();
+            
+                
+
+        }
+    }
+}
