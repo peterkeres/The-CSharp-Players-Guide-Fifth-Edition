682afa7f85e5d114fe9cd03219d272e2d946175d
Peter K <pkeresdev@gmail.com>
2023-08-22 12:40:26 -0400

    Adding in the "Watchtower" challenge

---
+++ Watchtower/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Watchtower // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int userX, userY;
+
+            Console.Write("Please enter X value: ");
+            userX = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Please enter Y value: ");
+            userY = Convert.ToInt32(Console.ReadLine());
+
+            string enemyPlacment = "";
+
+            if (userX < 0 && userY > 0)
+            {
+                enemyPlacment = "in the NW";
+            }
+            else if (userX == 0 && userY > 0)
+            {
+                enemyPlacment = "in the N";
+            }
+            else if (userX > 0 && userY > 0)
+            {
+                enemyPlacment = "in the NE";
+            }
+            else if (userX < 0 && userY == 0)
+            {
+                enemyPlacment = "in the W";
+            }
+            else if (userX == 0 && userY == 0)
+            {
+                enemyPlacment = "Here";
+            }
+            else if (userX > 0 && userY == 0)
+            {
+                enemyPlacment = "in the E";
+            }
+            else if (userX < 0 && userY < 0)
+            {
+                enemyPlacment = "in the SW";
+            }
+            else if (userX == 0 && userY < 0)
+            {
+                enemyPlacment = "in the S";
+            }
+            else if (userX > 0 && userY < 0)
+            {
+                enemyPlacment = "in the SE";
+            }
+
+            Console.WriteLine($"The enemy is {enemyPlacment}");
+            
+
+
+
+        }
+    }
+}
