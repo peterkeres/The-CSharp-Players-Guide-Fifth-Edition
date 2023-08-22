7a5f41bc6115c635d7371c6ba088bdfabdd821e5
Peter K <pkeresdev@gmail.com>
2023-08-22 09:54:49 -0400

    Adding in "Repairing the clocktower" challenge.

---
+++ Repairing the Clocktower/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Repairing_the_Clocktower // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            bool isEven = (userNumber % 2) == 0;
+
+            if (isEven)
+            {
+                Console.WriteLine("Tick");
+            }
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+
+
+        }
+    }
+}
