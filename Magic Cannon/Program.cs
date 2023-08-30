edbaeca8780cbbdccd5b459f26aea0bef3e330c9
Peter K <pkeresdev@gmail.com>
2023-08-29 21:41:41 -0400

    Adding in 'magic cannon' and 'prototype'

---
+++ Magic Cannon/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+
+                string gem;
+
+                
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    gem = "Fire and Electric";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (i % 3 == 0)
+                {
+                    gem = "Fire";
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (i % 5 == 0)
+                {
+                    gem = "Electric";
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    gem = "normal";
+                    Console.ResetColor();
+                }
+
+                
+                Console.WriteLine($"{i}: {gem}");
+
+
+
+            }
+            
+            
+            
+            
+            
+            
+        }
+    }
+}
---
+++ The Prototype/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int user1 , user2 ;
+
+
+            do
+            {
+                Console.Write("User 1, Enter a number between 0 and 100: ");
+                user1 = Convert.ToInt32(Console.ReadLine());
+                
+
+                if (user1 <=0 || user1 >= 100)
+                {
+                    Console.WriteLine($"Nope, {user1} isnt in the range im looking for");
+                }
+                
+            } while (user1 <=0 || user1 >= 100);
+
+
+            Console.Clear();
+            Console.WriteLine("User 2, guess the number.");
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                user2 = Convert.ToInt32(Console.ReadLine());
+
+                if (user2 > user1)
+                {
+                    Console.WriteLine($"{user2} was too high.");
+                }
+                else if (user2 < user1)
+                {
+                    Console.WriteLine($"{user2} was too low.");
+                }
+                
+            } while (user1 != user2);
+
+            Console.WriteLine("you guessed the number!");
+            
+
+        }
+    }
+}
