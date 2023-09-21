bb829f92919cbdae19774bda409325152882698f
Peter K <pkeresdev@gmail.com>
2023-09-20 20:37:52 -0400

    Added in replicator of dto and laws of freach

---
+++ Laws of Freach/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Laws_of_Freach // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] {4, 51, -7, 13, -99, 15, -8, 45, 90};
+
+
+            int total = 0;
+            int smallest = int.MaxValue;
+
+            foreach (var value in array)
+            {
+                if (value < smallest)
+                {
+                    smallest = value;
+                }
+                total += value;
+            }
+
+
+            Console.WriteLine($"Smallest Value is {smallest}");
+            float average = (float)total / array.Length;
+            Console.WriteLine($"With an average of {average}");
+
+
+
+        }
+    }
+}
---
+++ Replicator of DTo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Replicator_of_DTo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] array1 = new int[5];
+            int[] array2 = new int[array1.Length];
+
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine("Please Enter a number");
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array2[i] = array1[i];
+            }
+
+
+            Console.WriteLine("Show off arrays");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine($"Value {i+1}: array1 = {array1[i]} , array2 = {array2[i]}");
+            }
+
+
+
+        }
+    }
+}
