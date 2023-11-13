3529e744bb6c6f7c019dad150569daa8cfea20e3
Peter K <pkeresdev@gmail.com>
2023-11-13 12:18:54 -0500

    adding in 'CountDown'

---
+++ Countdown/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("starting countdown from 10!");
+            CountdownFrom(10);
+        }
+
+
+        static void CountdownFrom(int number)
+        {
+            Console.WriteLine($"{number}");
+            int newNumber = number - 1;
+
+            if (number <= 0)
+            {
+                return;
+            }
+            CountdownFrom(newNumber);
+        }
+        
+        
+        
+        
+    }
+}
