1849917e8af77b8277167992ca9f03bfe64c727c
Peter K <pkeresdev@gmail.com>
2024-02-06 18:14:08 -0500

    Added in both the point and the color

---
+++ Color/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Xml;
+
+namespace Color // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color1 = new Color(20, 20, 20);
+            Color color2 = Color.Green();
+
+            Console.WriteLine($"Color 1 is: (R {color1.R}, G {color1.G}, B {color1.B})");
+            Console.WriteLine($"Color 2 is: (R {color2.R}, G {color2.G}, B {color2.B})");
+        }
+    }
+
+
+    public class Color
+    {
+        public int R { get; private set; }
+        public int G { get; private set; }
+        public int B { get; private set; }
+
+
+        public Color(int r, int g, int b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+
+        public static Color White()
+        {
+            return new Color(255, 255, 255);
+        }
+
+        public static Color Black()
+        {
+            return new Color(0, 0, 0);
+        }
+
+        public static Color Red()
+        {
+            return new Color(0, 0, 0);
+        }
+
+        public static Color Orange()
+        {
+            return new Color(255, 165, 0);
+        }
+
+        public static Color Yellow()
+        {
+            return new Color(255, 255, 0);
+        }
+
+        public static Color Green()
+        {
+            return new Color(0, 128, 0);
+        }
+
+        public static Color Blue()
+        {
+            return new Color(0, 0, 255);
+        }
+
+        public static Color Purple()
+        {
+            return new Color(128, 0, 128);
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ Point/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Point // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(2, 3);
+            Point point2 = new Point(-4, 0);
+
+            Console.WriteLine($"point 1's coordinates are ({point1.X},{point1.Y})");
+            Console.WriteLine($"point 2's coordinates are ({point2.X},{point2.Y})");
+
+        }
+    }
+
+
+
+
+    class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+
+        public Point() : this(0, 0)
+        {
+            
+        }
+        
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    
+}
