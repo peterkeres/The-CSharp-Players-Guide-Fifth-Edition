68ea73d9566e7fd4fdbe8c80480a9b42b06bd22f
Peter K <pkeresdev@gmail.com>
2024-02-09 23:49:08 -0500

    Adding in 15-Puzzle

---
+++ 15-Puzzle/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _15_Puzzle // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+
+    public class Board
+    {
+        // keeps track of tiles
+        // tells manager when someone won
+    }
+
+    public class Player
+    {
+        // takes user input
+        // asks tiles to move'
+        // tells manager when move was made
+    }
+
+    public class Tiles
+    {
+        // takes input from user and sees if it can move that space on baord
+    }
+
+    public class GameManager
+    {
+        // displays items in the UI
+        // Calls boardmaker when making a new level
+    }
+
+    public class BoardMaker
+    {
+        // Just makes new boards at will
+    }
+    
+    
+    
+    
+}
