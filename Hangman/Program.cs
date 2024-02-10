ed184f7a3f3118d53b9d3b567243337c5f388702
Peter K <pkeresdev@gmail.com>
2024-02-10 00:12:08 -0500

    Adding in Hangman

---
+++ Hangman/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Hangman // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+
+
+    public static class WordMaker
+    {
+        // gets a word from a list
+    }
+
+    public class GameManager
+    {
+        // takes in guess and see if its apart of the word
+        // keeps track of when user wins
+    }
+
+    public class Player
+    {
+        // takes in user intpu
+    }
+
+    public class Ui
+    {
+        // shows the current state of the game to player
+    }
+    
+    
+    
+    
+    
+    
+    
+}
