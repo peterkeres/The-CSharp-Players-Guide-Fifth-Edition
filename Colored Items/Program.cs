c4ef2a034f974e6205c1780ce1fb33527ac770c1
Peter K <pkeresdev@gmail.com>
2024-02-21 22:24:11 -0500

    adding in Colored Items

---
+++ Colored Items/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Colored_Items // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+            ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+            ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+            
+            sword.Display();
+            bow.Display();
+            axe.Display();
+
+        }
+    }
+
+    class ColoredItem<T>
+    {
+        public T Item { get;}
+        public ConsoleColor ConsoleColor { get; }
+
+
+        public ColoredItem(T item,ConsoleColor consoleColor)
+        {
+            ConsoleColor = consoleColor;
+            Item = item;
+        }
+
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor;
+            Console.WriteLine(Item);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+        
+    }
+    
+    
+
+    class Sword
+    {
+        
+    }
+
+    class Bow
+    {
+        
+    }
+
+    class Axe
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
