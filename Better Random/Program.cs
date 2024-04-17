afeb3df00e30f8c385969d641a17994af6327330
Peter K <pkeresdev@gmail.com>
2024-04-17 18:34:51 -0400

    Adding in 'better random'

---
+++ Better Random/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            
+            
+            Console.WriteLine($"Double is : {random.NextDouble(9)}");
+            Console.WriteLine($"The next string is : {random.NextString("testing", "help", "cool")}");
+            Console.WriteLine($"the coin flip is : {random.CoinFlip(.7)}");
+        }
+    }
+}
---
+++ Better Random/RandomExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MyApp;
+
+public static class RandomExtensions
+{
+
+    public static double NextDouble(this Random random, int maxValue)
+    {
+        return random.Next(maxValue) + random.NextDouble();
+    }
+
+    public static string NextString(this Random random ,params string[] strings)
+    {
+        return strings[random.Next(strings.Length)];
+    }
+
+
+
+    public static bool CoinFlip(this Random random, double rateOfTrue = .5)
+    {
+        double flip = random.NextDouble();
+        return flip <= rateOfTrue ?  true : false ;
+        
+    }
+    
+    
+    
+}
